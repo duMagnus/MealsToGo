6d92c060afc315fda0cb1e1b5918e1f5c5c1cc97
Eduardo Magnus <eduj.magnus@gmail.com>
2023-09-13 12:21:47 -0300

    EsLint and Prettier

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <>
       <SafeAreaView style={styles.container}>
-        <StatusBar backgroundColor="#E0E1DD" barStyle={'dark-content'} />
+        <StatusBar backgroundColor="#E0E1DD" barStyle={"dark-content"} />
         <View style={styles.searchBar}>
           <Text>Search</Text>
         </View>
@@ -19,15 +20,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E0E1DD',
+    backgroundColor: "#E0E1DD",
   },
-  searchBar:{
-    backgroundColor: '#E0E1DD',
+  searchBar: {
+    backgroundColor: "#E0E1DD",
     padding: 20,
   },
   listContainer: {
-    backgroundColor: '#778DA9',
+    backgroundColor: "#778DA9",
     padding: 20,
     flex: 1,
-  }
+  },
 });
